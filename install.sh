657aae9856a7e030b51c74cfdef98ca1e3545c8a
Grctsamu <98705451+Grctsamu@users.noreply.github.com>
2023-07-05 15:15:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-md SchatterSerial
+mkdir SchatterSerial
 cd SchatterSerial
 curl https://raw.githubusercontent.com/Grctsamu/schatterserial/main/index.js >> index.js
 npm i serialport
-npm i socket-io-client
+npm i socket.io-client
