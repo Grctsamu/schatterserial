1ee581031b581019ae3baa6c5acca069b0dd8aad
samu63cs <98705451+samu63cs@users.noreply.github.com>
2024-01-04 19:33:10 -0700

    edit install.sh for my new profile name

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir SchatterSerial
 cd SchatterSerial
-curl https://raw.githubusercontent.com/Grctsamu/schatterserial/main/index.js >> index.js
+curl https://raw.githubusercontent.com/samu63cs/schatterserial/main/index.js >> index.js
 npm i serialport
 npm i socket.io-client
