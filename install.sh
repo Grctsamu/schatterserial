73749a2544b998d9bdca674ef61093dddae8898a
samu63cs <98705451+samu63cs@users.noreply.github.com>
2024-01-04 20:04:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,7 @@ curl https://raw.githubusercontent.com/samu63cs/schatterserial/main/index.js >> 
 npm i serialport
 npm i socket.io-client
 
-read -p "Do you want to add SchatterSerial to the crontab? (yes/no): " respuesta
 
-if [[ "$respuesta" =~ ^[Yy][Ee][Ss]$ ]]; then
-    # Logic to add the program to the crontab
-    (crontab -l ; echo "@reboot node $(pwd)/SchatterSerial/index.js") | crontab -
-    echo "Added to crontab"
-else
-    echo "Operation canceled."
-fi
 
 
 
